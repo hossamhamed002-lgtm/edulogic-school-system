14dd739f7099a3a2880e16b5cf6f1f53a1534f65
Hossam Hamed <hossamhamed002@gmail.com>
2025-12-31 17:55:36 +0200

    Update Cloudflare Pages catch-all proxy naming

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,12 +1,14 @@
 export async function onRequest({ request }) {
   const url = new URL(request.url);
 
-  const target =
-    "https://school-pay-pro.onrender.com" +
+  const backendBase = "https://school-pay-pro.onrender.com";
+
+  const targetUrl =
+    backendBase +
     url.pathname.replace("/api", "") +
     url.search;
 
-  return fetch(target, {
+  return fetch(targetUrl, {
     method: request.method,
     headers: request.headers,
     body: request.body
